e1d771566ab81a9ef4cdce5e76943010017b4cf6
jonny <js90@hml.com>
2024-01-25 15:40:17 +0200

    changes

--- src/layout/header/HeaderComponent.jsx
+++ src/layout/header/HeaderComponent.jsx
@@ -160,7 +160,7 @@ const HeaderComponent = ({ isDarkTheme, onThemeChange }) => {
             component="div"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
-            LOGO
+            LOG
           </Typography>
           <Links />
           <FilterComponent />
